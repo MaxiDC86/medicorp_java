b6f341793a78eb4fb779f28a51f9925031142968
maxidc86 <max-rub@hotmail.com>
2021-11-24 23:43:02 -0300

    Patient input data validation done.

--- src/medicorp_package/Medicorp.java
+++ src/medicorp_package/Medicorp.java
@@ -44,7 +44,7 @@ class miMarcoListar extends JFrame {
 	public miMarcoListar(ArrayList<paciente> losPacientes) {
 		this.losPacientes = losPacientes;
 		setBounds(100, 300, 600, 400);
-		setTitle("Bienvenidos a MEDICORP SOLUTIONS");
+		setTitle("MEDICORP SOLUTIONS - LISTADO DE PACIENTES");
 		setResizable(false);
 		setIconImage(null);
 		setVisible(true);
@@ -56,6 +56,10 @@ class miMarcoListar extends JFrame {
 				areaTexto.append("DNI: " + dni);
 				areaTexto.append(" Apellido: " + losPacientes.get(i).getLastName());
 				areaTexto.append(" Nombre:" + losPacientes.get(i).getName());
+				String year = Integer.toString(losPacientes.get(i).getBirthDate().get(Calendar.YEAR));
+				String month = Integer.toString(losPacientes.get(i).getBirthDate().get(Calendar.MONTH) + 1);
+				String day = Integer.toString(losPacientes.get(i).getBirthDate().get(Calendar.DAY_OF_MONTH));
+				areaTexto.append(" Fecha de Nacimiento: " + day + "/" + month + "/" + year);
 				areaTexto.append("\n");
 			}
 
@@ -84,20 +88,20 @@ class Lamina extends JPanel {
 
 	private class listar_pacientes implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			/*try {
-
-				for (int i = 0; i < losPacientes.size(); i++) {
-					System.out.println(losPacientes.get(i).getDNI());
-					System.out.println(losPacientes.get(i).getLastName());
-					System.out.println(losPacientes.get(i).getName());
-					System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.YEAR));
-					System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.MONTH) + 1);
-					System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.DAY_OF_MONTH));
-				}
-			} catch (NullPointerException erorr) {
-				System.out.println("No hay datos cargados");
-				JOptionPane.showMessageDialog(null, "No hay datos cargados");
-			}*/
+			/*
+			 * try {
+			 * 
+			 * for (int i = 0; i < losPacientes.size(); i++) {
+			 * System.out.println(losPacientes.get(i).getDNI());
+			 * System.out.println(losPacientes.get(i).getLastName());
+			 * System.out.println(losPacientes.get(i).getName());
+			 * System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.YEAR));
+			 * System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.MONTH) +
+			 * 1); System.out.println(losPacientes.get(i).getBirthDate().get(Calendar.
+			 * DAY_OF_MONTH)); } } catch (NullPointerException erorr) {
+			 * System.out.println("No hay datos cargados");
+			 * JOptionPane.showMessageDialog(null, "No hay datos cargados"); }
+			 */
 			miMarcoListar mimarco2 = new miMarcoListar(losPacientes);
 			mimarco2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -106,19 +110,39 @@ class Lamina extends JPanel {
 
 	private class alta_pacientes implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			dni = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el DNI del paciente: "));
-			apellido = JOptionPane.showInputDialog("Ingrese el apellido del paciente: ");
-			nombre = JOptionPane.showInputDialog("Ingrese el nombre del paciente: ");
-			fechaNacimiento.set(Calendar.YEAR,
-					Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de nacimiento: ")));
-			fechaNacimiento.set(Calendar.MONTH,
-					Integer.parseInt(JOptionPane.showInputDialog("Ingrese el mes de nacimiento: ")) - 1);
-			fechaNacimiento.set(Calendar.DAY_OF_MONTH,
-					Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dia de nacimiento: ")));
-			paciente socio = new paciente(dni, apellido, nombre, fechaNacimiento); // Se da de alta un nuevo socio
-			losPacientes.add(paciente.getSocioTotal() - 1, socio);
-			System.out.println("Se da de alta un paciente");
-
+			String regex = "^[a-zA-Z]+$";
+			try {
+				dni = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el DNI del paciente: "));
+				apellido = JOptionPane.showInputDialog("Ingrese el apellido del paciente: ");
+				if (apellido.matches(regex)) {// ...
+				}
+				apellido = apellido.trim();
+				apellido = apellido.substring(0, 1).toUpperCase() + nombre.substring(1);
+				nombre = JOptionPane.showInputDialog("Ingrese el nombre del paciente: ");
+				if (nombre.matches(regex)) {// ...
+				}
+				nombre = nombre.trim();
+				nombre = nombre.substring(0, 1).toUpperCase() + nombre.substring(1);
+				fechaNacimiento.setLenient(false);
+				fechaNacimiento.set(Calendar.YEAR,
+						Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de nacimiento: ")));
+				fechaNacimiento.set(Calendar.MONTH,
+						Integer.parseInt(JOptionPane.showInputDialog("Ingrese el mes de nacimiento: ")) - 1);
+				fechaNacimiento.set(Calendar.DAY_OF_MONTH,
+						Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dia de nacimiento: ")));
+				System.out.println(fechaNacimiento.getTime());
+				paciente socio = new paciente(dni, apellido, nombre, fechaNacimiento); // Se da de alta un nuevo socio
+				losPacientes.add(paciente.getSocioTotal() - 1, socio);
+				System.out.println("Se da de alta un paciente");
+				JOptionPane.showMessageDialog(null, "Paciente dado de alta satisfactoriamente.");
+			} catch (NumberFormatException er) {
+				System.out.println("Error: " + er);
+				JOptionPane.showMessageDialog(null, "Error: Carga de datos cancelada.");
+			} catch (IllegalArgumentException er2) {
+				JOptionPane.showMessageDialog(null, "Error: Carga de fecha de nacimiento.");
+			} catch (NullPointerException er3) {
+				JOptionPane.showMessageDialog(null, "Error: Carga de datos cancelada.");
+			}
 		}
 	}
 
