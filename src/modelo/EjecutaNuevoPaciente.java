90aa92f70f643ff622c9bf2059ce4e0e3a57b550
maxidc86 <max-rub@hotmail.com>
2021-12-10 00:46:51 -0300

    clean up of the projects useless files

--- src/modelo/EjecutaNuevoPaciente.java
+++ src/modelo/EjecutaNuevoPaciente.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
-import java.time.LocalTime;
+
 
 public class EjecutaNuevoPaciente {
 	public EjecutaNuevoPaciente() {
